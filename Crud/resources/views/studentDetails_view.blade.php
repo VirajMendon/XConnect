ff09ddcc3bddb32c1d2e14afefccc78dd928bd47
VirajMendon <viraj.m@xpheno.com>
2019-02-24 16:26:23 +0530

    no message

--- Crud/resources/views/studentDetails_view.blade.php
+++ Crud/resources/views/studentDetails_view.blade.php
@@ -29,6 +29,7 @@
 							<a class="dropdown-item" href="{{route('course.create')}}">Add Course</a>
 							<div class="dropdown-divider"></div>
 							<a class="dropdown-item" href="#">Add Marks</a>
+							<a class="dropdown-item" href="#">Add Marks</a><a class="dropdown-item" href="#">Add Marks</a><a class="dropdown-item" href="#">Add Marks</a>
 						</div>
 					</li>
 				</ul>
